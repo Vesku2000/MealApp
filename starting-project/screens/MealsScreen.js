de4ca2c699701fac73d5e060f2ac28f2b6faef32
Valtteri Syrjanen <valtterisyrjanen@gmail.com>
2023-10-22 19:59:08 +0300

    adding files

---
+++ starting-project/screens/MealsScreen.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native"
+import { MEALS } from "../data/dummy-data"
+
+function MealsScreen() {
+    return <View style={styles.container}>
+        <Text>
+            Meals Overview
+        </Text>
+    </View>
+}
+
+export default MealsScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    }
+});
